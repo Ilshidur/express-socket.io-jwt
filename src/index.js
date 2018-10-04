1256e8b2734ccc7052c58210f44034dfe298f8d2
Nicolas Coutin <Ilshidur@users.noreply.github.com>
2018-10-04 11:11:50 +0200

    refactor: big refactor

--- src/index.js
+++ src/index.js
@@ -4,75 +4,83 @@ const { isEqual: ipEquals } = require('ip');
 // Default input functions
 const defaultJwtFromRequest = ({ cookie, queryParams }) => cookie.jwt || queryParams.token;
 const defaultConnectionNameFromRequest = () => null;
-const defaultOnSocketParseError = (err, socket, next) => {
-  return next(new Error('Could not decode socket token'));
-}
+const defaultOnSocketParseError = (err, socket, next) => next(new Error('Could not decode socket token'));
 
-const indexSockets = sockets => sockets.reduce((acc, socketId) => {
-  const socket = namespace.connected[socketId];
-  return { ...acc, [socket.connectionName]: socket };
-}, {});
+const indexSockets = sockets => sockets.reduce((acc, socket) => ({
+  ...acc, [socket.connectionName]: socket,
+}), {});
 
 const authenticateSocketMiddleware =
   io =>
-  ({
-    matchSocket = 'ip',
-    required = true,
-  }) =>
-  async (req, res, next) => {
-    function filterSockets(req, namespace, filterFunc) {
-      return Object.values(namespace.connected).filter(socket => filterFunc(req, socket));
-    }
+    ({
+      matchSocket = 'ip',
+      required = true,
+    }) =>
+      async (req, res, next) => {
+        function filterSockets(request, namespace, filterFunc) {
+          return Object.values(namespace.connected).filter(socket => filterFunc(request, socket));
+        }
 
-    let sockets;
-    try {
-      const namespace = io.of('/');
+        let sockets;
+        try {
+          const namespace = io.of('/');
 
-      switch (matchSocket) {
-        case 'ip':
-          sockets = filterSockets(req, namespace, (req, socket) => ipEquals(req.ip, socket.handshake.address));
-          break;
-        case 'cookie':
-          sockets = filterSockets(req, namespace, (req, socket) => req.cookies ? req.cookies.io === socket.id : false);
-          break;
-        default:
-          sockets = filterSockets(req, namespace, matchSocket);
-      }
-      console.log(sockets);
-    } catch (err) {
-      return next(err);
-    }
+          switch (matchSocket) {
+            case 'ip':
+              sockets = filterSockets(
+                req,
+                namespace,
+                (request, socket) => ipEquals(request.ip, socket.handshake.address),
+              );
+              break;
+            case 'cookie':
+              sockets = filterSockets(
+                req,
+                namespace,
+                (request, socket) => (request.cookies ? request.cookies.io === socket.id : false),
+              );
+              break;
+            default:
+              sockets = filterSockets(
+                req,
+                namespace,
+                matchSocket,
+              );
+          }
+        } catch (err) {
+          return next(err);
+        }
 
-    if (sockets.length === 0 && required) {
-      return next(new Error('Unauthorized'));
-    }
+        if (sockets.length === 0 && required) {
+          return next(new Error('Unauthorized'));
+        }
 
-    const indexedSockets = indexSockets(sockets);
+        const indexedSockets = indexSockets(sockets);
 
-    const validateSockets = sockets.map(async (socket) => {
-      if (!required) {
-        return;
-      }
+        const validateSockets = sockets.map(async (socket) => {
+          if (!required) {
+            return;
+          }
 
-      if (socket && !socket.token && required) {
-        throw new Error('Unauthorized');
-      }
+          if (socket && !socket.token && required) {
+            throw new Error('Unauthorized');
+          }
 
-      if (socket && !socket.payload && required) {
-        throw new Error('Unauthorized');
-      }
-    });
+          if (socket && !socket.payload && required) {
+            throw new Error('Unauthorized');
+          }
+        });
 
-    try {
-      await Promise.all(validateSockets);
-    } catch (err) {
-      return next(err);
-    }
+        try {
+          await Promise.all(validateSockets);
+        } catch (err) {
+          return next(err);
+        }
 
-    req.getSocket = (connection) => connection ? indexedSockets[connection] : sockets[0];
+        req.getSocket = connection => (connection ? indexedSockets[connection] : sockets[0]);
 
-    next();
-  };
+        return next();
+      };
 
 async function parseSocketJwt(socket, { jwtFromRequest, secret, connectionNameFromRequest }) {
   const cookie = socket.request;
@@ -81,8 +89,7 @@ async function parseSocketJwt(socket, { jwtFromRequest, secret, connectionNameFr
   // TODO: Option for the request to wait for the socket to prepare
 
   socket.token = await jwtFromRequest({ cookie, queryParams }, socket);
-  socket.secret = typeof secret === 'function' ? await secret(req) : secret;
-  socket.payload = jwt.verify(socket.token, socket.secret);
+  socket.payload = socket.token ? jwt.verify(socket.token, secret) : null;
   socket.connectionName = await connectionNameFromRequest({ cookie, queryParams }, socket);
 }
 
@@ -110,7 +117,7 @@ const socketMiddleware = ({
 // TODO: Allow choice of namespaces to '.use' (string or function)
 const createMiddleware = (io, opts = {}) => {
   if (!io) {
-    return next(new Error('Missing socket.io server'));
+    throw new Error('Missing socket.io server');
   }
 
   io.use(socketMiddleware(opts));
